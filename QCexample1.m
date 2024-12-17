5e98a959a3bcfa6c0d26a69ae294f9003beb352f
skylartreegrey <skylar.grey@ufl.edu>
2024-12-17 14:18:08 -0500

    Update QCexample1.m

--- QCexample1.m
+++ QCexample1.m
@@ -7,6 +7,6 @@ p1 = plot(t,a,'LineColor',[0 0.4470 0.7410]);%plot first y-variable
 hold on%Need this so we can add a second plot
 p2 = plot(t,b,'LineColor',[0.8500 0.3250 0.0980]);%plot second y-variable
 p = [p1;p2];%Combines both plots into one object so we can make the legend
-lgd = legend(p,'sin(x)','T(x)','Location','N');%Makes the legend
+lgd = legend(p,'sin(x)','T(x)','Location','S');%Makes the legend
 set(lgd,'FontSize','16'); %Changes legend font size
 hold off%Because we turned hold on above, we need to then turn it off
